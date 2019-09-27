288615f2b9d77b62525c075a5608816acf2e7e74
Daniel <u1201416@unimilitar.edu.co>
2019-09-26 20:21:06 -0500

    json

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const mongo = require('mongoose');
-var bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-
 
 var schema = mongo.Schema({
     name: String,
